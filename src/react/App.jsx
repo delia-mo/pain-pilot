3f8814c63c1c2bc2fffa047b539a714d8b0683eb
Delia <talviphoenix@gmail.com>
2025-06-12 20:50:56 +0200

    Feat: Add color palette, modify createTheme()

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -10,6 +10,39 @@ import { createTheme, ThemeProvider } from '@mui/material/styles'
 import AppLayout from './AppLayout'
 
 const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      light: '#28278a',
+      main: '#1e1c87',
+      dark: '#1b1a55',
+      contrastText: '#cac7fc'
+    },
+    secondary: {
+      light: '#3e5094',
+      main: '#3e5094',
+      dark: '#3e5094',
+      contrastText: '#cac7fc'
+    },
+    background: {
+      default: '#070f2b',
+      paper: '#1b1a55'
+    },
+    text: {
+      primary: '#cac7fc',
+      secondary: '#9290c3'
+    }
+  },
+  typography: {
+    fontFamily: '"Roboto", sans-serif',
+    h4: {
+      fontWeight: 600
+    },
+    body1: {
+      fontSize: '1rem'
+    }
+  },
+
   components: {
     MuiCssBaseline: {
       styleOverrides: defaultTheme => ({
--- src/react/AppRoutes.jsx
+++ src/react/AppRoutes.jsx
@@ -1,25 +1,16 @@
 import React from 'react'
-
-import { Route, Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Home from './Pages/Home'
-import CatNames from './Pages/CatNames/CatNames'
-import Profile from './Pages/Profile/Profile'
-import ProfileOverview from './Pages/Profile/ProfileOverview'
-import ProfileSettings from './Pages/Profile/ProfileSettings'
-
 import Error404 from './Pages/Error404'
+import TemplateScreen from './Pages/TemplateScreen'
 
-const AppRoutes = () => (
+const AppRoute = () => (
   <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/profile" element={<Profile />}>
-      <Route path="" element={<ProfileOverview />} />
-      <Route path="settings" element={<ProfileSettings />} />
-    </Route>
-    <Route path="/catnames" element={<CatNames />} />
+    <Route path="/template" element={<TemplateScreen />} />
     <Route path="*" element={<Error404 />} />
   </Routes>
 )
 
-export default AppRoutes
+export default AppRoute
