fa5d4890b2240a11fda2687756f93607afcffc10
Delia <talviphoenix@gmail.com>
2025-07-21 12:18:39 +0200

    feat: hook for localstorage

--- src/hooks/useMigraineData.js
+++ src/hooks/useMigraineData.js
@@ -1,36 +1,43 @@
 import { useState, useEffect } from 'react'
 
-const parseEntry = (entryString) => {
-  const parts = entryString.replace('[ ', '').replace(' ]', '').split(';')
-  const type = parts[0].trim()
-  const data = {}
-  for (let i = 1; i < parts.length; i++) {
-    const [key, value] = parts[i].split(':')
-    data[key.trim()] = value.trim()
-  }
-
-  return { type, ...data }
-}
-
 const useMigrainData = () => {
   const [entries, setEntries] = useState([])
 
+  const readAllEntries = () => {
+    const parsed = []
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i)
+      if (key.startsWith('migraine-')) {
+        try {
+          const data = JSON.parse(localStorage.getItem(key))
+          if (data) parsed.push({ type: 'logging', ...data })
+        } catch (e) {
+          console.warn('Fehler beim Parsing von Migräne-Daten mit Key ', key)
+        }
+      }
+      if (key.startsWith('tracking-')) {
+        try {
+          const data = JSON.parse(localStorage.getItem(key))
+          if (data) parsed.push({ type: 'tracking', ...data })
+        } catch (e) {
+          console.warn('Fehler beim Parsen von Tacking-Daten mit Key ', key)
+        }
+      }
+    }
+    return parsed
+  }
+
   useEffect(() => {
-    const rawData = JSON.parse(localStorage.getItem('migraineData') || '[]')
-    const parsed = rawData.map(parseEntry)
-    setEntries(parsed)
+    setEntries(readAllEntries())
   }, [])
 
   const refresh = () => {
-    const rawData = JSON.parse(localStorage.getItem('migraineData') || '[]')
-    const parsed = rawData.map(parseEntry)
-    setEntries(parsed)
+    setEntries(readAllEntries())
   }
 
-  const addEntry = (entry) => {
-    const current = JSON.parse(localStorage.getItem('migraineData') || '[]')
-    current.push(entry)
-    localStorage.setItem('migraineData', JSON.stringify(current))
+  const addEntry = (type, data) => {
+    const key = `${type}-${data.date}`
+    localStorage.setItem(key, JSON.stringify(data))
     refresh()
   }
 
